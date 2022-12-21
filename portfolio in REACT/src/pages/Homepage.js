4ef9af56885fd47434e0998903d18f09d9314299
solabenet <solabenet@gmail.com>
2022-12-21 08:29:50 -0800

    init commit

---
+++ portfolio in REACT/src/pages/Homepage.js
@@ -0,0 +1,282 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Form, Button } from "react-bootstrap";
+import "./Homepage.css";
+import datepicker from '../components/Seceduling'
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      <img className="cont-1-icon" alt="" src="../cont-1@2x.png" />
+      <div className="rectangle-div" />
+      <img className="ellipse-icon" alt="" src="../ellipse-2.svg" />
+      <img className="rectangle-icon" alt="" src="../rectangle-1@2x.png" />
+      <div className="rectangle-div1" />
+      <img className="ellipse-icon1" alt="" src="../ellipse-1.svg" />
+      <img className="main-2-icon" alt="" src="../main-2@2x.png" />
+     
+      <div className="designer">DESIGNER</div>
+      <div className="e-talent">ICOG</div>
+      <div className="generation-z">Ampol Technologies</div>
+      <div className="bemmy-technology">Cache POS</div>
+      <div className="multiple-hotels">
+        <p className="dec">Amran</p>
+        <p className="hotels">HOTELS</p>
+      </div>
+      <div className="meta">META</div>
+      <div className="lets-connect">
+        <p className="dec">LET’S</p>
+        <p className="hotels">CONNECT</p>
+      </div>
+      <div className="uiux-design">Python programming</div>
+      <div className="logo-design">Developer and project manger</div>
+      <div className="web-site-development">WEB-SITE DEVELOPMENT & co-founder</div>
+      <div className="consultation">Website developer and manger</div>
+      <div className="front-end-development">FRONT-END DEVELOPMENT</div>
+      <div className="introduction-to-front-end-deve">
+        <p className="dec">INTRODUCTION TO</p>
+        <p className="hotels">FRONT-END DEVELOPMENT</p>
+      </div>
+      <div className="programming-with-javascript">
+        <p className="dec">PROGRAMMING WITH</p>
+        <p className="hotels">JAVASCRIPT</p>
+      </div>
+      <div className="advanced-html-css">
+        <p className="dec">ADVANCED</p>
+        <p className="hotels">{`HTML & CSS`}</p>
+      </div>
+      <Form.Group className="rectangle-formgroup">
+        <Form.Control type="text" placeholder="Input placeholder" />
+      </Form.Group>
+      <div className="version-control">VERSION CONTROL</div>
+      <div className="for-anything-regarding">FOR ANYTHING REGARDING:</div>
+      
+      <div className="i-am-a-creative">I AM A CREATIVE</div>
+      <div className="want-to-learn-more">WANT TO LEARN MORE?</div>
+      <div className="programmer">PROGRAMMER</div>
+      <button className="component-2" id="contact">
+        <div className="rectangle-div2" />
+        <b className="contact-me">CONTACT ME</b>
+      </button>
+      <button className="component-3">
+        <div className="rectangle-div3" />
+        <b className="contact-me1">CONTACT ME</b>
+      </button>
+      <div className="hello-i-am-abenet">
+        <span>{`HELLO, I AM `}</span>
+        <span className="abe">Abenet</span>
+      </div>
+      <div className="projects">PROJECTS</div>
+      <div className="certifications-earned">CERTIFICATIONS EARNED</div>
+      <img className="ux-1-icon" alt="" src="../ux-1@2x.png" />
+      <div className="Abenet">Abenet Solomon</div>
+      <div className="i-design-revolutionary-designs">
+        <p className="dec">
+          <span>
+            I DESIGN REVOLUTIONARY DESIGNS AND REPUTABLE SOFTWARES THAT MAKE AN
+            IMPACT ON BOTH THE CLIENT AND THE DESIGN COMMUNITY.
+          </span>
+        </p>
+        <p className="dec">
+          <span>
+            I BELIEVE THAT THE CREATIONS THAT MAKE THE GREATEST RIPPLE EFFECT
+            THROUGHOUT HISTORY ARE ONES’ THOUGHT TO BE EITHER TO SILLY OR JUST
+            IMPOSSIBLE.
+          </span>
+        </p>
+        <p className="dec">
+          <span className="as-a-designer">
+            AS A DESIGNER AND A SOFTWARE DEVELOPER
+          </span>
+          <span>{`, I PLAN TO TACKLE THE BIGGEST AND SOMEHOW IMPOSSIBLE SEEMING PROBLEMS KNOWN TO THE `}</span>
+          <span className="world">WORLD</span>
+          <span className="span">.</span>
+        </p>
+        <p className="and-i-shall-do-so">
+          <span>AND I SHALL DO SO</span>
+        </p>
+      </div>
+      <div className="music-art-friendship-some">
+        <p className="dec">
+        </p>
+        <p className="dec">
+          <span>&nbsp;</span>
+        </p>
+        <p className="dec">
+          <span>&nbsp;</span>
+        </p>
+        <p className="hotels">
+          <span className="music-art-friendship">I</span>
+          <span>
+            {" "}
+            am a Developer based in Ethiopia. My sepcialities are designing and
+            programming 
+          </span>
+        </p>
+      </div>
+      <b className="b">{`&`}</b>
+      <div className="frame-div">
+        <b className="home">HOME</b>
+      </div>
+      <div className="frame-div1">
+        <b className="home">ABOUT</b>
+      </div>
+      <div className="frame-div2">
+        <b className="home">PROJECTS</b>
+      </div>
+      <div className="frame-div3">
+        <b className="home">CERTIFICATIONS</b>
+      </div>
+      <div className="frame-div4">
+        <b className="home">CONTACT</b>
+      </div>
+      <img className="img-black-1" alt="" src="../img-black-1@2x.png" />
+      <div className="line-div" />
+      <div className="rectangle-div4" />
+      <img className="ellipse-icon2" alt="" src="../ellipse-11.svg" />
+      <button className="component-4" id="details">
+        <div className="rectangle-div5" />
+        <b className="see-details">SEE DETAILS</b>
+      </button>
+      <img className="logo-1-icon" alt="" src="../logo-1@2x.png" />
+      <div className="rectangle-div6" />
+      <img
+        className="programming-1-icon"
+        alt=""
+        src="../programming-1@2x.png"
+      />
+      
+      <div className="rectangle-div7" />
+      <img className="idea-1-icon" alt="" src="../idea-1@2x.png" />
+      <div className="rectangle-div8" />
+      <div id="demo-booking-single"></div>
+      <img className="meta-intro-1" alt="" src="../meta-intro-1@2x.png" />
+      <img className="javascript-1-icon" alt="" src="../javascript-1@2x.png" />
+      <img className="depth-html-1" alt="" src="../depth-html-1@2x.png" />
+      <img className="version-1-icon" alt="" src="../version-1@2x.png" />
+      <a
+        className="frame-a"
+        href="mailto:solabenet@gmail.com?subject=I am looking for a software developer"
+        id="sware"
+      >
+        <div className="component-1">
+          <div className="rectangle-div3" />
+          <b className="software-dev">SOFTWARE DEV.</b>
+        </div>
+      </a>
+      <a
+        className="frame-a1"
+        href="mailto:solabenet@gmail.com?subject=i need an app developed, let's talk."
+        id="app"
+      >
+        <div className="component-11">
+          <div className="rectangle-div3" />
+          <b className="software-dev">APP DEVELOPMENT</b>
+        </div>
+      </a>
+      <a
+        className="frame-a2"
+        href="mailto:solabenet@gmail.com?subject=i am looking to get my website developed , let's talk."
+        id="web"
+      >
+        <div className="component-12">
+          <div className="rectangle-div3" />
+          <b className="software-dev">WEB-SITE DEV</b>
+        </div>
+      </a>
+      <a
+        className="frame-a3"
+        href="mailto:solabenet@gmail.com?subject=i am looking for a front-end developer"
+        id="fend"
+      >
+        <div className="component-13">
+          <div className="rectangle-div3" />
+          <b className="software-dev">FRONT-END DEV.</b>
+        </div>
+      </a>
+      <a
+        className="frame-a4"
+        href="mailto:solabenet@gmail.com?subject=i need you to create or rebrand a logo."
+        id="logo"
+      >
+        <div className="component-14">
+          <div className="rectangle-div3" />
+          <b className="software-dev">LOGO DESIGN</b>
+        </div>
+      </a>
+      <a
+        className="frame-a5"
+        href="mailto:solabenet@gmail.com?subject=i need a banner designed."
+        id="banner"
+      >
+        <div className="component-15">
+          <div className="rectangle-div3" />
+          <b className="software-dev">BANNER DESIGN</b>
+        </div>
+      </a>
+      <a
+        className="frame-a6"
+        href="mailto:solabenet@gmail.com?subject=are you the one that does branding? i've got a job for you."
+        id="brand"
+      >
+        <div className="component-16">
+          <div className="rectangle-div3" />
+          <b className="software-dev">BRANDING</b>
+        </div>
+      </a>
+      <a
+        className="frame-a7"
+        href="mailto:solabenet@gmail.com?subject=i am in need of consulting."
+        id="consult"
+      >
+        <div className="component-17">
+          <div className="rectangle-div3" />
+          <b className="software-dev">CONSULTING</b>
+        </div>
+      </a>
+     
+      <img
+        className="mingcutetelegram-fill-icon"
+        alt=""
+        src="../mingcutetelegramfill.svg"
+      />
+      <img
+        className="icbaseline-facebook-icon"
+        alt=""
+        src="../icbaselinefacebook.svg"
+      />
+      <img
+        className="jamtwitter-circle-icon"
+        alt=""
+        src="../jamtwittercircle.svg"
+      />
+      <img
+        className="riinstagram-fill-icon"
+        alt=""
+        src="../riinstagramfill.svg"
+      />
+      <div className="line-div1" />
+      <div className="line-div2" />
+      <div className="line-div3" />
+      <div className="line-div4" />
+      <div className="copyright">COPYRIGHT</div>
+      <div className="abenet-dev">Abenet development</div>
+      <img
+        className="ricopyright-fill-icon"
+        alt=""
+        src="../ricopyrightfill.svg"
+      />
+      <img className="mditm-icon" alt="" src="../mditm.svg" />
+      <img className="vector-icon" alt="" src="../vector-1.svg" />
+      <img className="vector-icon1" alt="" src="../vector-2.svg" />
+      <div className="rectangle-div18" />
+      <div className="rectangle-div19" />
+      <Button className="component-5" variant="outline-primary">
+        submit
+      </Button>
+      <b className="what-are-your-thought-on-my-we">
+        What are your thought on my website?
+      </b>
+    </div>
+  );
+};
+
+export default Homepage;
